a1089199d5c3372f71dda0fbba64d7de5154e07e
Camo282 <camo282@gmail.com>
2022-10-25 15:21:41 -0400

    setting up About in components

---
+++ src/components/Navigation/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h1>Former Educator turned Full Stack Developer</h1>
+      <hr/>
+      <img src={'./assets/IMG_0594.jpg'} alt="John Sheridan"/>
+      <p className="content is-italic mt-4">
+        My name is John Sheridan, after 18 years in education, 8 as a school administrator, I am studying to be a full stack developer through the University of Richmond's Coding Bootcamp.
+      </p>
+      <p className="content">
+        I plan on writing something here that is super exciting!
+      </p>
+    </section>
+  );
+}
+
+export default About;
